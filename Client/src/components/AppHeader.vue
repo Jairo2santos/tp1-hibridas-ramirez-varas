ddf8d8a22ac4c59fc3dcaa14aca0af6f5e0dfe06
Matias Alejo Varas <mativaras71@gmail.com>
2023-10-07 20:29:57 -0300

    estilos Nav

--- Client/src/components/AppHeader.vue
+++ Client/src/components/AppHeader.vue
@@ -3,24 +3,22 @@
   <div class="bg-white shadow-md">
     <div class="container mx-auto px-4 py-2">
       <div class="flex items-center justify-between">
-        <div class="text-xl font-semibold text-purple-900">
+        <div class="flex items-center text-xl font-semibold text-purple-900">
           <a href="/">
             <img src="../assets/logo-curseek.png" alt="logo curseek" width="220">
           </a>
+          <router-link to="/cursos" class="ml-4 hover:bg-purple-200 px-3 py-1 rounded transition text-lg">Cursos</router-link>
         </div>
         <nav class="space-x-4">
-          <router-link to="/" class="hover:bg-purple-200 px-3 py-1 rounded transition">Home</router-link>
-          <router-link to="/cursos" class="hover:bg-purple-200 px-3 py-1 rounded transition">Cursos</router-link>
-          <router-link to="/usuarios" class="hover:bg-purple-200 px-3 py-1 rounded transition">Login</router-link>
+          <router-link to="/usuarios" class="hover:bg-purple-200 px-3 py-1 rounded transition text-lg font-bold">Login</router-link>
         </nav>
       </div>
     </div>
   </div>
 </template>
-
   
-  <script>
+<script>
   export default {
     name: "AppHeader"
   };
-  </script>
+</script>
